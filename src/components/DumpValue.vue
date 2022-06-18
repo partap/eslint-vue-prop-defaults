36f7bd42954dd4407250ef1f2945154f774ce441
Partap Davis <partapdavis@gmail.com>
2022-06-17 18:12:59 -0600

    initial commit

---
+++ src/components/DumpValue.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  val?: unknown
+  flag?: boolean
+}
+const props = withDefaults(defineProps<Props>(), {
+  flag: false
+})
+
+</script>
+<template>
+  <div>
+    <div>Type: {{ typeof props.val }}</div>
+    <div>Value: {{ JSON.stringify(props.val) }} </div>
+  </div>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <DumpValue :val="{foo: 'blah'}" />
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import DumpValue from 'components/DumpValue.vue'
+</script>
